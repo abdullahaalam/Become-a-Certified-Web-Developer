9c264577482514e1eddee82c4a30f05ee3cd0503
Abdullah A. Alam <abdullah.alam.omi@gmail.com>
2016-09-19 00:43:16 -0700

    Added Chapter 1 PHP Lab Exercise

---
+++ ch1-php-lab/ch1-php-lab.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>PHP Lab Exercise 1</title>
+    </head>
+    <body>
+        <?php
+            echo "Honda Accord";
+            echo ("<br/>Ford Explorer");
+            echo "<br/>";
+
+            printf("<br/>0%0.2f", 12.99);
+            printf("<br/>00%0.2f", 5.85);
+            printf("<br/>%0.2f", 100.25);
+            printf("<br/>%0.2f", 765.25);
+            printf("<br/>0%0.2f", 78.15);
+        ?>
+    </body>
+</html>
